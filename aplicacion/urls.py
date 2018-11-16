0d69e4a47bd6d9115525bcb452dc8cb5e90337c0
EmilioLizama <emilio.lizama@ug.uchile.cl>
2018-11-15 22:12:31 -0300

    Se implementaron los cursos en el landing page alumno

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login, name='login')
+    url(r'^$', views.login, name='login'),
+    url(r'^landingpagealumnos/', views.landingPageAlumnos, name = 'landingpagealumnos')
 ]
 
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from aplicacion.models import Curso, AlumnoCurso, PersonaNatural, Alumno
+
 
 def login(request):
     return render(request, 'login.html')
+
+
+def landingPageAlumnos(request):
+    persona = PersonaNatural.objects.get(RUT="19.540.088-1")
+    alumno = Alumno.objects.get(personaNatural=persona)
+    cursos = AlumnoCurso.objects.filter(alumno=alumno)
+
+    context = {'cursos': cursos}
+    return render(request, 'home-vista-alumno.html', context)
