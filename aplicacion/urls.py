bab77beb70572b7cc030d16f0638bf784b717230
plt1994 <patricio.lopez.t@ug.uchile.cl>
2018-11-17 19:54:24 -0300

    magregada vista de perfil alumno a urls y links en perfil-vista-dueno

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/', views.login_view, name='login'),
-    url(r'^perfil/', views.perfilAlumnos_view, name='perfilalumnos'),
-    url(r'^logout/', views.logout_view, name='logout'),
     url(r'^$', views.landingPageAlumnos_view, name='landingpagealumnos'),
+
+    url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
+
+    url(r'^perfil/', views.perfilAlumnos_view, name='perfildueno'),
     url(r'^coevaluacionAlumnos/', views.coevaluacionAlumnos_view, name='coevaluacionAlumnos')
 ]
