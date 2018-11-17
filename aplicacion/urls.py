15855869ebb6c074b022a6856767ad8fba547175
EmilioLizama <emilio.lizama@ug.uchile.cl>
2018-11-16 22:21:05 -0300

    Lista la vista alumnos con logout

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^login/', views.login_view, name='login'),
+    url(r'^logout/', views.logout_view, name='logout'),
     url(r'^$', views.landingPageAlumnos_view, name='landingpagealumnos')
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from aplicacion.models import Curso, AlumnoCurso, Alumno
+from aplicacion.models import Curso, AlumnoCurso, Alumno, Coevaluacion
 
 
 def login_view(request):
@@ -20,9 +21,26 @@ def login_view(request):
 
 
 def landingPageAlumnos_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        alumno = Alumno.objects.filter(user=user)
+        if not alumno:
+            return redirect('login')
+        else:
+            alumnocurso = AlumnoCurso.objects.filter(alumno=alumno[0]).values('curso_id')
+            cursos = Curso.objects.filter(id__in=alumnocurso)
+            coevaluaciones = Coevaluacion.objects.filter(curso__in=cursos).order_by('-fecha_inicio')
+            context = {'cursos': cursos, 'user': user, 'coevaluaciones': coevaluaciones}
+            return render(request, 'home-vista-alumno.html', context)
+    else:
+        return redirect('login')
     # persona = PersonaNatural.objects.get(RUT="19.540.088-1")
     # alumno = Alumno.objects.get(personaNatural=persona)
     # cursos = AlumnoCurso.objects.filter(alumno=alumno)
     # context = {'cursos': cursos}
     # return render(request, 'home-vista-alumno.html', context)
-    return render(request, 'home-vista-alumno.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
