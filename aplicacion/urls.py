d8f331303b7ab0695ed4f703cd8238152151864b
Felipe Lizama <felipealonsolr@gmail.com>
2018-11-18 23:05:23 -0300

    Comentado lo que falta

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.landingPageAlumnos_view, name='landingpagealumnos'),
     url(r'^coevaluacionAlumnos/', views.coevaluacionAlumnos_view, name='coevaluacionAlumnos'),
     url(r'^perfil/', views.perfil_view, name='perfil'),
-    url(r'^curso-alumno/', views.curso_alumno, name='cursoAlumnos')
+    url(r'^curso-alumno/', views.curso_alumno, name='cursoAlumnos')#falta agregarle el curso_id, no se como hacerlo con url(), con path() si
 ]
