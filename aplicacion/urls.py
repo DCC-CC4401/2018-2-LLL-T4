a26e5c27ef862072af6206b7cd4cc4b33f978f12
Felipe Lizama <felipealonsolr@gmail.com>
2018-11-20 12:08:00 -0300

    Agregada la url de la ficha de curso vista por un docente

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^coevaluacionAlumnos/', views.coevaluacionAlumnos_view, name='coevaluacionAlumnos'),
     url(r'^perfil/', views.perfil_view, name='perfil'),
     url(r'^curso-alumno/', views.curso_alumno, name='cursoAlumnos')#falta agregarle el curso_id, no se como hacerlo con url(), con path() si
+    url(r'^curso-docente/', views.curso_docente, name='cursoDocente')
 ]
