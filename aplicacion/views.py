8925dcf1e07b82c27e8b8390fafbb2e8c54647a2
plt1994 <patricio.lopez.t@ug.uchile.cl>
2018-11-21 16:15:35 -0300

    vista notas perfil alumno funcionando

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-from aplicacion.models import Curso, AlumnoCurso, Alumno, Coevaluacion, Grupo, AlumnoGrupo
+
+from aplicacion.models import Curso, AlumnoCurso, Alumno, Coevaluacion, Grupo, AlumnoGrupo, AlumnoCoevaluacion
 
 
 def login_view(request):
@@ -74,11 +75,15 @@ def perfil_view(request):
         if not alumno:
             return redirect('login')
         else:
+
             alumnocurso = AlumnoCurso.objects.filter(alumno=alumno[0]).values('curso_id')
             cursos = Curso.objects.filter(id__in=alumnocurso)
+            alumnonotas = AlumnoCoevaluacion.objects.filter(alumno=alumno[0])
+            n_cursos = cursos.__len__()
             coevaluaciones = Coevaluacion.objects.filter(curso__in=cursos).order_by('-fecha_inicio')
-            #falta preguntar por las notas
-            context = {'cursos': cursos, 'user': user, 'coevaluaciones': coevaluaciones}
+            n_coev = coevaluaciones.__len__()
+            context = {'cursos': cursos, 'user': user, 'coevaluaciones': coevaluaciones, 'n_cursos': n_cursos,
+                       'notas': alumnonotas, 'n_coev': n_coev}
             return render(request, 'perfil-vista-dueno.html', context)
     else:
         return redirect('login')
--- static/js/coeval.js
+++ static/js/coeval.js
@@ -32,17 +32,51 @@ function changePass() {
     document.getElementById("notas-resumen").style.display = "none";
     document.getElementById("notas-placeholder").style.display = "none";
     document.getElementById("change-pass-btn").classList.add("active");
-    document.getElementById("row-btn").classList.remove("active");
+    //document.getElementById("row-btn").classList.remove("active");
 }
 
-function showNotas() {
+function loadCurso(id_curso) {
+    cursocodigo = document.getElementById("cursocodigo"+id_curso).textContent;
+    cursoano = document.getElementById("cursoano"+id_curso).textContent;
+    return cursocodigo+', '+cursoano;
+}
+
+function showNotas(id_curso, n_cursos, n_coev) {
+    //alert(n_coev);
+    document.getElementById("currentCurso").innerHTML = loadCurso(id_curso);
     document.getElementById("cambiar-contrasena").style.display = "none";
     document.getElementById("notas-resumen").style.display = "block";
     document.getElementById("notas-placeholder").style.display = "none";
-    document.getElementById("row-btn").classList.add("active");
+    //document.getElementById("row-btn"+id_curso).classList.add("active");
+
+    //alert(id_curso);
+    for(i = 0; i<= n_cursos; i++){
+        //alert('hola');
+        for(j = 0; j<=n_coev; j++){
+            idval=i+"-"+j;
+            //alert("agregar " + idval);
+            if (i === id_curso) {
+                try{
+                    //alert("agregar " + idval);
+                    document.getElementById(idval).style.display = "table-row";
+                    document.getElementById("row-btn"+id_curso).classList.add("active");
+                }catch{}
+            } else {
+                //alert("remover " + i);
+                try {
+                    document.getElementById(idval).style.display = "none";
+                    //document.getElementById("row-btn" + id_curso).classList.remove("active");
+                } catch {
+                }
+            }
+        //alert("endfor")
+        }
+    }
+
     var changePass = document.getElementById("change-pass-btn");
     if (changePass !== null) changePass.classList.remove("active");
 }
+
 
 function cancelPass() {
     document.getElementById("cambiar-contrasena").style.display = "none";
